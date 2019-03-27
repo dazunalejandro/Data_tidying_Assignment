7bfa2047587a284857827f08ce2a9bc019ac17ed
UCM3Miguel <manovill@espol.edu.ec>
2019-03-27 10:34:07 -0500

    commit

--- DataTidyingProject/app.R
+++ DataTidyingProject/app.R
@@ -33,30 +33,7 @@ ui <- navbarPage(h3("Movie Industry Analysis"),
                             )
                           ) # sidebarLayout
                  ) # fluidPage
-<<<<<<< HEAD
             )# Panel 1
-=======
-            ),# Panel 1
-            tabPanel(h4("Critics vs Viewer"),
-                     fluidPage( 
-                       sidebarLayout(# position = "right",
-                         sidebarPanel(
-                           checkboxGroupInput("checkGroup2", 
-                                              h4("Select Popularity Level"), 
-                                              choices =vote_choices,
-                                              selected ="Popular"),
-                           checkboxGroupInput("checkGroup3", 
-                                              h4("Facet by Genre"), 
-                                              choices =list_choices,
-                                              selected ="Comedy")
-                         ),
-                         mainPanel(
-                           plotOutput(outputId = "value2")
-                         )
-                       )
-                     )
-            )
->>>>>>> 62302dcaa5b59eb05d8c5e491039bd3b77b00fb0
             
 )
 
@@ -64,7 +41,6 @@ ui <- navbarPage(h3("Movie Industry Analysis"),
 
 # Define server logic required to draw a histogram
 server <- function(input, output, session) {
-  
   
   
   
@@ -89,40 +65,7 @@ server <- function(input, output, session) {
       geom_line(size=1)+
       geom_point(size=2) 
   })
-  
-<<<<<<< HEAD
-  
-    
 
-=======
-  ########################################
-  ####################Panel 2
-  ########################################
-  
-  
-  output$value2 <- renderPlot({ 
-    
-    
-    movies_subset <- movies_1016 %>%
-      filter(votesFactor %in% input$checkGroup2) %>%
-      filter(genre %in% input$checkGroup3)
-    
-    ggplot(movies_subset) +
-      aes(x=score,fill=votesFactor,color=votesFactor) +
-      scale_colour_discrete(name="Popularity Level")+
-      facet_grid(~genre)+
-      labs(x="Score",y="Density") +
-      theme(plot.title = element_text(size=20,hjust = 0.5),
-            axis.title=element_text(size=17),
-            legend.text = element_text(size = 17),
-            legend.title = element_text(size = 17)) +
-      stat_density(geom = "line",size=0.5) 
-     #geom_bar()
-  })
-  
-  
- 
->>>>>>> 62302dcaa5b59eb05d8c5e491039bd3b77b00fb0
 }
 
 
