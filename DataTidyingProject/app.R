4ef1db64869b3ad22e09be229b91260e756968bf
Alejandro Dazun <dazunalejandro@gmail.com>
2019-03-27 16:04:52 +0100

    Some Comments

--- DataTidyingProject/app.R
+++ DataTidyingProject/app.R
@@ -31,15 +31,38 @@ ui <- navbarPage(h3("Movie Industry Analysis"),
                                 plotOutput(outputId = "value1")
                               
                             )
-                          ) # fluidPage
-                 )
-            )#  titlePage
+                          ) # sidebarLayout
+                 ) # fluidPage
+            ),# Panel 1
+            tabPanel(h4("Critics  vs Viewers"),
+                     fluidPage( 
+                       sidebarLayout(# position = "right",
+                         sidebarPanel(
+                           checkboxGroupInput("checkGroup2", 
+                                              h4("Select Popularity Level"), 
+                                              choices =vote_choices,
+                                              selected ="Popular"),
+                           checkboxGroupInput("checkGroup3", 
+                                              h4("Facet by three Genre"), 
+                                              choices =list_choices,
+                                              selected ="Comedy")
+                         ),
+                         mainPanel(
+                           plotOutput(outputId = "value2")
+                         )
+                       )
+                     )
+            )
 )
 
 
 
 # Define server logic required to draw a histogram
 server <- function(input, output, session) {
+  
+  ########################################
+  ####################Panel 1
+  #######################################
   
   output$value1 <- renderPlot({
     movies_subset <- movies_1016 %>%
@@ -51,6 +74,8 @@ server <- function(input, output, session) {
       ggtitle("Yearly Gross Income") +
       aes(x=year,y=grossPergenre,fill=genre,color=genre) +
       labs(x="Year",y="Gross Income") +
+      scale_colour_discrete(name="Genre") +
+      scale_fill_discrete(name="Genre") +
       theme(plot.title = element_text(size=20,hjust = 0.5),
             axis.title=element_text(size=17),
             legend.text = element_text(size = 17),
@@ -60,8 +85,33 @@ server <- function(input, output, session) {
       geom_line(size=1)+
       geom_point(size=2) 
   })
+  
+  ########################################
+  ####################Panel 2
+  ########################################
+  output$value2 <- renderPlot({ 
+    
+    
+    movies_subset <- movies_1016 %>%
+      filter(votesFactor %in% input$checkGroup2) %>%
+      filter(genre %in% input$checkGroup3)
+    
+    ggplot(movies_subset) +
+      aes(x=score,fill=votesFactor,color=votesFactor) +
+      scale_colour_discrete(name="Popularity Level")+
+      facet_grid(~genre)+
+      labs(x="Score",y="Density") +
+      theme(plot.title = element_text(size=20,hjust = 0.5),
+            axis.title=element_text(size=17),
+            legend.text = element_text(size = 17),
+            legend.title = element_text(size = 17)) +
+      stat_density(geom = "line",size=0.5) 
+      #geom_bar()
+    
+    
+  })
 }
-?theme
+
 
 # Run the application 
 shinyApp(ui = ui, server = server)
