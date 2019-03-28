e3f2138aa53fd3d0184acca6b685c33f015f855d
UCM3Miguel <manovill@espol.edu.ec>
2019-03-28 12:54:51 -0500

    Change title panel 4

--- DataTidyingProject/app.R
+++ DataTidyingProject/app.R
@@ -287,10 +287,10 @@ server <- function(input, output, session) {
     palette(c("#E41A1C", "#377EB8", "#4DAF4A", "#984EA3",
               "#FF7F00", "#FFFF33", "#A65628", "#F781BF", "#999999"))
     
-    par(mar = c(5.1, 4.1, 0, 1))
+    #par(mar = c(5.1, 4.1, 0, 1))
     plot(selectedData(),
          col = clusters()$cluster,
-         pch = 20, cex = 3)
+         pch = 20, cex = 3, main = "Movies by Group")
     #paste(round(100*m, 2), "%", sep="")
     
     #points(clusters()$centers, pch = 4, cex = 4, lwd = 4)
